1a8e2a9d4f96e38c59bb330bcdc42cce4d23db7c
Agustin P <agustinpadron94@gmail.com>
2023-11-02 03:35:14 -0300

    Implementacion de Pausa (P)

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def game_loop_scene() -> None:
     # Gameloop
     move_repeat_delay = 200  # Sensibilidad, cuanto mayor mas lento se mueve
     last_move_time = 0
+    game_pause = False
 
     while not Grid.end:
         clock.tick(FPS)
@@ -33,33 +34,42 @@ def game_loop_scene() -> None:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     end_game()
+                if event.key == pygame.K_p:
+                    game_pause = not game_pause
                 if event.key == pygame.K_RIGHT:
-                    Grid.move(1,0)
-                    last_move_time = pygame.time.get_ticks()
+                    if (not game_pause):
+                        Grid.move(1,0)
+                        last_move_time = pygame.time.get_ticks()
                 if event.key == pygame.K_LEFT:
-                    Grid.move(-1,0)
-                    last_move_time = pygame.time.get_ticks()
+                    if (not game_pause):
+                        Grid.move(-1,0)
+                        last_move_time = pygame.time.get_ticks()
                 if event.key == pygame.K_DOWN:
-                    pygame.time.set_timer(timer_event, 50)
+                    if (not game_pause):
+                        pygame.time.set_timer(timer_event, 50)
                 if event.key == pygame.K_SPACE:
-                    Grid.rotate()
+                    if (not game_pause):
+                        Grid.rotate()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_DOWN:
                     pygame.time.set_timer(timer_event, time_delay)
             elif event.type == timer_event:
-                Grid.move(0,1)
+                if (not game_pause):
+                    Grid.move(0,1)
 
         # Manejo de movimiento continuo
         if pygame.key.get_pressed()[pygame.K_LEFT]:
-            current_time = pygame.time.get_ticks()
-            if current_time - last_move_time >= move_repeat_delay:
-                Grid.move(-1, 0)
-                last_move_time = current_time
+            if (not game_pause):
+                current_time = pygame.time.get_ticks()
+                if current_time - last_move_time >= move_repeat_delay:
+                    Grid.move(-1, 0)
+                    last_move_time = current_time
         if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            current_time = pygame.time.get_ticks()
-            if current_time - last_move_time >= move_repeat_delay:
-                Grid.move(1, 0)
-                last_move_time = current_time
+            if (not game_pause):
+                current_time = pygame.time.get_ticks()
+                if current_time - last_move_time >= move_repeat_delay:
+                    Grid.move(1, 0)
+                    last_move_time = current_time
                 
 
 
